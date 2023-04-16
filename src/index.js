c1e11cddffa0e3967ae4fd89cb35f34ba96e5ef8
Penelope Drago <prd@drago.com.au>
2023-04-16 15:50:39 +1000

    fetch and display data

--- src/index.js
+++ src/index.js
@@ -1,5 +1,27 @@
 const init = () => {
-  
+  // Select DOM element 'form'
+  const inputForm = document.querySelector('form');
+
+  // Add event listener to form
+  inputForm.addEventListener('submit', (event) => {
+    
+    // Override default form behaviour
+    event.preventDefault();
+    
+    // Access input value
+    const input = document.querySelector('input#searchByID');
+        
+    // Fetch data based on user input
+    fetch(`http://localhost:3000/movies/${input.value}`)
+    .then(response => response.json())
+    .then(data => {
+      const title = document.querySelector('section#movieDetails h4');
+      const summary = document.querySelector('section#movieDetails p');
+
+      title.textContent = data.title;
+      summary.textContent = data.summary;
+    });
+  });
 }
 
 document.addEventListener('DOMContentLoaded', init);
